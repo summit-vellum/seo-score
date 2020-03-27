f5912666578038bacc05681bd5383d2d6c6f847b
Gien Mangaliman <Gien.Mangaliman@summitmedia.com.ph>
2020-03-27 08:50:20 +0800

    seo score update

--- src/views/checkSeo.blade.php
+++ src/views/checkSeo.blade.php
@@ -1,4 +1,4 @@
 <input type="hidden" value="{{ json_encode(config('seoscore')) }}" data-scoring>
-<li>
-	@button(['element'=>'button', 'color'=>'blue','label'=>'Check SEO Score', 'attr'=>arrayToHtmlAttributes(['data-toggle' => 'modal', 'data-target' => '#toolModal', 'data-url' => url('/post/seo-score')]), 'class' => 'btn btn-primary mr-3 mt-2 px-5'])
+<li class="mr-1">
+	@button(['element'=>'button', 'color'=>'blue','label'=>'Check SEO Score', 'attr'=>arrayToHtmlAttributes(['data-toggle' => 'modal', 'data-target' => '#toolModal', 'data-url' => url('/post/seo-score')]), 'class' => 'btn btn-primary btn-block mt-2 mb-2 px-5'])
 </li>
